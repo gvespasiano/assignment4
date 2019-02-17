6c8305a521e2f056550a9c99c1e7b5d5d450302e
gvespasiano <gvespasiano@gmail.com>
2019-02-17 10:16:23 -0600

    JS Data

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
+//Data
+var finest = {
+  author:"Winston Churchill",
+  year:"1940",
+  BCE:false,
+}
+var india = {
+  author:"Ghandi",
+  year:"1942",
+  BCE:false,
+}
+var philippic = {
+  author:"Demosthenes",
+  year:"342",
+  BCE:true,
+}
+
+//Console Logs
 document.getElementById('BtnDonate').addEventListener('click', function(){
-  //Code in here executes when the user clicks the "Donate" button.
+  console.log("There are"+ "speaches on the page.");//Code in here executes when the user clicks the "Donate" button.
 });
 
 document.getElementById('BtnChurchill').addEventListener('click', function(){
